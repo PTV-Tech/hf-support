6eddd96cc6f055d242a7354c6a3f05cc25f974b5
Vinh Pham <vinhpt2@vng.com.vn>
2022-12-26 21:13:05 +0700

    update

--- js/scripts.js
+++ js/scripts.js
@@ -63,5 +63,24 @@ $(document).ready(function() {
     ],
   });
 
+  //Modal
+  $("div[id^='Modal']").each(function(){
+  
+    var currentModal = $(this);
+    
+    //click next
+    currentModal.find('.btn-next').click(function(){
+      currentModal.modal('hide');
+      currentModal.closest("div[id^='Modal']").nextAll("div[id^='Modal']").first().modal('show'); 
+    });
+    
+    //click prev
+    currentModal.find('.btn-prev').click(function(){
+      currentModal.modal('hide');
+      currentModal.closest("div[id^='Modal']").prevAll("div[id^='Modal']").first().modal('show'); 
+    });
+  
+  });  
+
 });
 
