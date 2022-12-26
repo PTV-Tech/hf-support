2077cf1c54aaddf36126a1f580c53a6652dac4bc
Vinh Pham <vinhpt2@vng.com.vn>
2022-12-26 22:27:47 +0700

    Test swipe and click keyboard

--- js/scripts.js
+++ js/scripts.js
@@ -79,8 +79,70 @@ $(document).ready(function() {
       currentModal.modal('hide');
       currentModal.closest("div[id^='Modal']").prevAll("div[id^='Modal']").first().modal('show'); 
     });
-  
-  });  
-
+  });
 });
+
+const modalsInFrame1 = document.getElementsByClassName('frame1');
+
+window.onkeydown = e => {
+   let open = false;
+   let currentIndex = 0;
+   for (let item of modalsInFrame1) {
+     currentIndex++
+     if (item.classList.contains('show')) {
+       open = true;
+       break;
+     }
+   }
+   if (!open) {
+     return;
+   }
+  e = e || window.event;
+  if (e.keyCode == '37' && currentIndex > 1) {
+    $(`#Modal${currentIndex}`).modal('hide');
+    $(`#Modal${currentIndex - 1}`).modal('show');
+  } else if (e.keyCode == '39' && currentIndex < 4) {
+    $(`#Modal${currentIndex}`).modal('hide');
+    $(`#Modal${currentIndex + 1}`).modal('show');
+  }
+}
+
+let touchstartX = 0
+let touchendX = 0
+
+function checkDirection(currentIndex) {
+  if (touchendX < touchstartX && currentIndex < 4) {
+    // alert('swiped left!')
+    $(`#Modal${currentIndex}`).modal('hide');
+    $(`#Modal${currentIndex + 1}`).modal('show');
+  }
+  if (touchendX > touchstartX && currentIndex > 1) {
+    // alert('swiped right!')
+    $(`#Modal${currentIndex}`).modal('hide');
+    $(`#Modal${currentIndex - 1}`).modal('show');
+  }
+}
+
+document.addEventListener('touchstart', e => {
+  touchstartX = e.changedTouches[0].screenX
+})
+
+document.addEventListener('touchend', e => {
+  touchendX = e.changedTouches[0].screenX
+  let open = false;
+  let currentIndex = 0;
+  for (let item of modalsInFrame1) {
+    currentIndex++
+    if (item.classList.contains('show')) {
+      open = true;
+      break;
+    }
+  }
+  if (!open) {
+    return;
+  }
+  checkDirection(currentIndex)
+})
+
+
 
